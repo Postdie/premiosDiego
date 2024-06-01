6c0dbaf32ef0e3c01be276d7cd6a3d8cff82136e
Postdie <fernandezdiego845@gmail.com>
2024-06-01 11:05:13 -0600

    1.5 TestingDeViews

--- premiosDiegoapp/polls/tests.py
+++ premiosDiegoapp/polls/tests.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.test import TestCase
+from django.urls.base import reverse
 from django.utils import timezone
 
 from .models import Question
@@ -11,3 +12,11 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(question_text="¿¿Quien esta en modelo dual?", pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """If no questions exist, an appropriate message is displayed"""
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
